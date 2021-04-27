4c69c0c92dc8b2f4af8d5b2f22d328b7de7f797e
Admin <Admin@DESKTOP-O7TDM5R.lan>
2021-04-27 14:27:30 -0700

    Pieces starter code

--- Connect 4/src/Pieces.java
+++ Connect 4/src/Pieces.java
@@ -1,6 +1,26 @@
+import java.awt.Image;
 
 public class Pieces {
 	
+	private int value;
+	private Image img;
+
+	public Pieces(boolean isRed) {
+		if(isRed) { 
+			value = 1;
+			img = getImage("red poker chip.png");
+		}
+		if(!isRed) {
+			value = 2;
+			img = getImage("black poker chip.png");
+		}
+
+	}
+
+	private Image getImage(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 }
