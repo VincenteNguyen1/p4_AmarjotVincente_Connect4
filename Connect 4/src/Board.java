507071fc844f31e60157e1502370c13788b4beb7
Admin <Admin@DESKTOP-O7TDM5R.lan>
2021-05-11 15:19:19 -0700

    I changed up all of it a bit, but now it works like a game a bit.
    I also added in some temporary methods so that you could print the board
    and add pieces.

--- Connect 4/src/Board.java
+++ Connect 4/src/Board.java
@@ -1,4 +1,102 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Board {
+public class Board extends JPanel implements MouseListener {
+	//change the size of the board if you need to
+	private int len = 6;
+	private int width = 7;
+	private static Piece[][] board;
+	
+	/* constructor for MainPain class */
+	public Board() {
+		
+//		f.setLayout(new GridLayout(rows,cols));
 
+		JFrame f = new JFrame("Example Drawing");
+		f.setSize(800,600); //width and height
+		f.add(this);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+		board = new Piece[len][width];
+		
+		for(int r = 0; r < board.length; r++) {
+			for(int c = 0; c < board[0].length; c++) {
+				board[r][c] = new Piece();
+			}
+		}
+	}
+	
+	public void addPiece(int col) {
+		int r = board.length-1;
+		while(board[r][col].getValue() > 0) {
+			if(r - 1 < 0) {
+				System.out.println("Cannot Place Piece Here");
+				return;
+			}
+			r--;
+		}
+		board[r][col].setPiece();
+		Piece.turnCntr++;
+	}
+	
+	public static void printBoard() {
+		for(int r = 0; r < board.length; r++) {
+			for(int c = 0; c < board[0].length; c++) {
+				System.out.print(board[r][c].getValue() + " ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public Piece[][] getBoard() {
+		return board;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+			//On a click, figure out what tile was clicked
+//			Tile theTile = (Tile) arg0.getComponent();
+//			System.out.println(theTile.r);
+			
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 }
+	
+
--- Connect 4/src/Piece.java
+++ Connect 4/src/Piece.java
@@ -6,26 +6,40 @@ import java.net.URL;
 public class Piece {
 	
 	private int value;
-	private static int turnCntr;//counts turns; but this means one side will always go first
+	public static int turnCntr;//counts turns; but this means one side will always go first
 	private Image img;
 	private int x, y;
 	private AffineTransform tx = AffineTransform.getTranslateInstance(x, y);
 
 	public Piece() {
-		if(turnCntr % 2 == 0) {//RED CHIP
-			value = 1;//value for red
-			img = getImage("red poker chip.png");
-			init(100, 100);
-		}
-		if(turnCntr % 2 != 0) {//BLACK CHIP
-			value = 2;//value for black
-			img = getImage("black poker chip.png");
-		}
-		turnCntr++;
+		value = 0;
 	}
 	
 	public int getValue() {
 		return value;
+	}
+	
+	public void setValue() {
+		if(turnCntr%2 == 0) value = 1;
+		if(turnCntr%2 == 1) value = 2;
+	}
+	
+	public void setImage() {
+		if(turnCntr % 2 == 0) {//RED CHIP
+			//value for red
+			img = getImage("red poker chip.png");
+			init(100, 100);
+		}
+		if(turnCntr % 2 != 0) {//BLACK CHIP
+			//value for black
+			img = getImage("black poker chip.png");
+			init(100, 100);
+		}
+	}
+	
+	public void setPiece() {
+		setValue();
+		setImage();
 	}
 	
 	public static int getTurnCntr() {//Static method so you don't need to use an object --> "Piece.getTurnCntr()"
@@ -52,6 +66,9 @@ public class Piece {
 	/*
 	 * A Paint method maybe or a way to display all of the images 
 	 * 
+	 * Change the method so that when a piece is created it's value is set to zero instead of of 1 or two,
+	 * and then make a setter for the value using the turncounter
 	 * 
+	 * add getter and setter for turncntr
 	 */
 }
