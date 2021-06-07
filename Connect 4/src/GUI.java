6fdece7f9d4e70f183de7e2d274625f35cf7e6af
Admin <Admin@DESKTOP-O7TDM5R.lan>
2021-06-07 14:55:06 -0700

    Added new StartScreen class.

--- Connect 4/src/GUI.java
+++ Connect 4/src/GUI.java
@@ -38,58 +38,63 @@ public class GUI extends JPanel implements ActionListener, MouseListener {
 	}
 	
 	public void paint(Graphics g) {
-		//if(isActive) board.paint(g);
-		//screen.paint(g);
-		board.paint(g);
+		screen.paint(g);
+		//board.paint(g);
 	}
 	
 	public GUI() {
-		
 		window = new JFrame ();
-		window.setSize(900,900);
+		window.setSize(885,729);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//	window.getContentPane().setBackground(Color.black);;
-	//	window.setLayout(null);
 		window.add(this);
 		t = new Timer(16, this);
 		t.start();
-		
 		window.setVisible(true);
-	//	con =  window.getContentPane();
-//		
-//		titlePanel = new JPanel ();
-//		titlePanel.setBounds(50, 50, 400, 75);
-//		titlePanel.setBackground(Color.white);
-//		title =  new JLabel("CONNECT 4");
-//		title.setForeground(Color.white);
-//		title.setFont(titleFont);
-//		
-//		startPanel = new JPanel ();
-//		startPanel.setBounds(600, 300, 100, 100);
-//		startPanel.setBackground(Color.black);
-//		
-//	//	startButton = new JButton("Start");
-//		startButton.setBackground(Color.black);
-//		startButton.setForeground(Color.white);
-//		startButton.setFont(ButtonFont);
-//		
-//		titlePanel.add(title);
-//		con.add(titlePanel);
-	//	con.add(startPanel);
-	//	startPanel.add(startButton);
-		
-		//;window.add(this);
-		
 	}
 	
-	public void gameScreen() {
-			
-	}
-	
-	public void mousePressed(MouseEvent arg0)
-	{	
-		System.out.println("asdasd");
+	@Override
+	public void mousePressed(MouseEvent arg0) {	
 		
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		repaint();
+		
+	}
+
+}
+
+
+
+//////////////
+
+/*
 		int x = arg0.getX();//sets variable x equal to mouse x position
 		
 		if(isActive)
@@ -132,76 +137,5 @@ public class GUI extends JPanel implements ActionListener, MouseListener {
 		}
 		board.addPiece(0);
 		board.printBoard();
-	}
+ */
 
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-//		int x = arg0.getX();//sets variable x equal to mouse x position
-//		if(isActive)
-//		{
-//			//Column 1
-//			if(x >= 0 && x < 25) 
-//			{
-//				board.addPiece(0);
-//			}
-//			//Column 2
-//			if(x >= 25 && x < 50) 
-//			{
-//				board.addPiece(1);
-//			}
-//			//Column 3
-//			if(x >= 50 && x < 75) 
-//			{
-//				board.addPiece(2);
-//			}
-//			//Column 4
-//			if(x >= 75 && x < 100) 
-//			{
-//				board.addPiece(3);
-//			}
-//			//Column 5
-//			if(x >= 100 && x < 125) 
-//			{
-//				board.addPiece(4);
-//			}
-//			//Column 6
-//			if(x >= 125 && x < 150) 
-//			{
-//				board.addPiece(5);
-//			}
-//			//Column 6
-//			if(x >= 150 && x < 175) 
-//			{
-//				board.addPiece(6);
-//			}
-//		}
-//		board.printBoard();
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		repaint();
-	}
-	
-
-}
--- Connect 4/src/StartScreen.java
+++ Connect 4/src/StartScreen.java
@@ -3,22 +3,34 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
 import java.net.URL;
 
 public class StartScreen {
 
 	private static boolean isActive = true;
 	private String path = "connectFourName.png";
-	private Image img;
+	private Image img = null;
+	private Image img2 = null;
+	private int x = 9, y = -25;
 	
 	public StartScreen() {
 		img = getImage(path);
 	}
 	
 	public void paint(Graphics g) {
+		if(img == null) img = getImage(path);
+		if(img2 == null) img2 = getImage("Board.png");
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.red);
+		g2.setColor(Color.gray);
 		g2.fillRect(0, 0, 900, 900);
+		g2.setColor(Color.white);
+		g2.fillRect(0, 0, 900, 230);
+		g2.drawImage(img, x, y, 850, 300, null);
+		g2.setColor(Color.white);
+		g2.fillOval(70, 280, 300, 150);
+		g2.fillOval(70, 480, 300, 150);
+		g2.drawImage(img2, 445, 295, 350, 300, null);
 	}
 	
 	public static void setIsActive(boolean a) {
