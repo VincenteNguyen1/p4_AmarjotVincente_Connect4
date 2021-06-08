1b47f4c1843e9c2b9068919ea04a46df72d9214a
Admin <Admin@DESKTOP-O7TDM5R.lan>
2021-06-08 14:53:26 -0700

    imageTest

---
+++ Connect 4/src/ImageTest.java
@@ -0,0 +1,49 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+public class ImageTest extends Applet
+{
+	
+	ImageTest test = new ImageTest();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////
+	
+	private Image img = null;
+	
+	public void paint(Graphics g) {
+		
+		this.setSize(680, 440);
+		
+		if (img ==  null) img = getImage("board.png");
+	
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, 20, 20, 20, 20, this);   
+		
+	}
+	
+	public Image getImage(String path)
+	{
+		Image tempImage = null; 
+		
+		try 
+		{
+			URL url = ImageTest.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(url);
+		}
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		return tempImage;
+	}
+
+}
